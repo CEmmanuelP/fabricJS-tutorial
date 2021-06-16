948dd0fa2a16ccb6f960a3e8b3e27e70d583247c
CEmmanuelP <chrisp3@daum.net>
2021-06-16 15:24:36 +0900

    commit3

--- src/App.js
+++ src/App.js
@@ -7,42 +7,50 @@ import { fabric } from "fabric";
 const App = () => {
   const [canvas, setCanvas] = useState("");
   const [imgURL, setImgURL] = useState("");
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef();
 
   const canvasRef = useRef();
+  const canvasRef2 = useRef();
 
   useEffect(() => {
     setCanvas(initCanvas());
   }, []);
 
-  // canvas객체 생성
-  const initCanvas = () =>
-    new fabric.Canvas("canvas", {
-      height: 300,
-      width: 500,
-      backgroundColor: "pink",
-    });
-
-  const addImg = (e, url, canvi) => {
-    e.preventDefault();
-    const img = new Image();
-    img.crossOrigin = "anonymous";
-    img.src = url;
-    img.onload = function () {
-      canvi.setBackgroundImage(img.src, canvi.renderAll.bind(canvi), {
-        originX: "left",
-        originY: "top",
-        left: 0,
-        top: 0,
-        crossOrigin: "anonymous",
-      });
-    };
-    // new fabric.Image.fromURL(url, (img) => {
-    //   img.scale(0.75);
-    //   canvi.add(img);
-    //   canvi.renderAll();
-    //   setImgURL("");
-    // });
+  const togglePlay = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
   };
+
+  // canvas객체 생성
+  const initCanvas = () => new fabric.Canvas("canvas");
+
+  // const addImg = (e, url, canvi) => {
+  //   e.preventDefault();
+  //   const img = new Image();
+  //   img.crossOrigin = "anonymous";
+  //   img.src = url;
+  //   img.onload = function () {
+  //     canvi.setBackgroundImage(img.src, canvi.renderAll.bind(canvi), {
+  //       originX: "left",
+  //       originY: "top",
+  //       left: 0,
+  //       top: 0,
+  //       crossOrigin: "anonymous",
+  //     });
+  //   };
+  //   new fabric.Image.fromURL(url, (img) => {
+  //     img.scale(0.75);
+  //     canvi.add(img);
+  //     canvi.renderAll();
+  //     setImgURL("");
+  //   });
+  // };
 
   // 범위 지정 사각형 생성 함수
   const createBoundary = (canvas) => {
@@ -69,6 +77,7 @@ const App = () => {
         top: y,
         fill: "rgb(255, 255, 255, 0)",
         stroke: "blue",
+        opacity: 1,
         strokeWidth: 4,
         strokeDashArray: [15, 15],
       });
@@ -103,10 +112,40 @@ const App = () => {
       square = canvas.getActiveObject();
       canvas.add(square);
 
-      cutImage(canvas, square);
+      imageCapture(square);
+      // cutImage(canvas, square);
+      // setVideo(canvas, square);
 
       canvas.renderAll();
     });
+  };
+
+  const imageCapture = (square) => {
+    console.log(canvasRef.current);
+    console.log(square);
+    console.log(videoRef.current);
+    let w, h, ratio;
+
+    // ratio = videoRef.current.width / videoRef.current.height;
+    w = square.width;
+    h = square.height;
+
+    // canvasRef2.current.width = w;
+    // canvasRef2.current.height = h;
+
+    const ctx = canvasRef2.current.getContext("2d");
+    // ctx.fillRect(0, 0, w, h);
+    ctx.drawImage(
+      videoRef.current,
+      square.left,
+      square.top,
+      w,
+      h,
+      square.left,
+      square.top,
+      w,
+      h
+    );
   };
 
   const clearCanvas = (canvas) => {
@@ -119,78 +158,87 @@ const App = () => {
     });
   };
 
-  const cutImage = (canvas, square) => {
-    const image = canvas.toDataURL({
-      format: "jpeg",
-      left: square.left,
-      top: square.top,
-      width: square.width + 2,
-      height: square.height + 2,
-    });
-    console.log(image);
-  };
+  // const cutImage = (canvas, square) => {
+  //   const image = canvas.toDataURL({
+  //     format: "jpeg",
+  //     left: square.left,
+  //     top: square.top,
+  //     width: square.width + 2,
+  //     height: square.height + 2,
+  //   });
+  //   console.log(image);
+  // };
 
-  const setVideoOnCanvas = (canvas) => {
-    const videoEl = document.querySelector("#video");
-    const video = new fabric.Image(videoEl, {
-      left: 0,
-      top: 0,
-      width: 500,
-      height: 300,
-      angle: 0,
-      originX: 0,
-      originY: 0,
-      objectCaching: true,
-      statefullCache: true,
-    });
+  // const setVideoOnCanvas = (canvas) => {
+  //   const videoEl = document.querySelector("#video");
+  //   const video = new fabric.Image(videoEl, {
+  //     left: 0,
+  //     top: 0,
+  //     width: 500,
+  //     height: 300,
+  //     angle: 0,
+  //     originX: 0,
+  //     originY: 0,
+  //     objectCaching: true,
+  //     statefullCache: true,
+  //   });
 
-    canvas.add(video);
-    fabric.util.requestAnimFrame(function render() {
-      canvas.renderAll();
-      fabric.util.requestAnimFrame(render);
-    });
-  };
+  //   canvas.add(video);
+  //   fabric.util.requestAnimFrame(function render() {
+  //     canvas.renderAll();
+  //     fabric.util.requestAnimFrame(render);
+  //   });
+  // };
 
-  const setVideo = (canvas) => {
+  const setVideo = (canvas, square) => {
     const video = document.querySelector("video");
 
-    drawImage(canvas, video);
+    drawImage(canvas, video, square);
   };
 
-  const drawImage = (canvas, video) => {
-    canvas
+  const drawImage = (canvas, video, square) => {
+    console.log(square.top);
+    let a = canvas
       .getContext("2d", { alpha: false })
-      .drawImage(video, 0, 0, canvas.width, canvas.height);
+      .drawImage(video, square.left, square.top, square.width, square.height);
+
+    console.log(a);
   };
 
   return (
-    <div>
-      <button onClick={() => setVideoOnCanvas(canvas)}>newSetVideo</button>
+    <div className="containerVideo">
       <button onClick={() => setVideo(canvas)}>setVideo</button>
       <button onClick={() => createBoundary(canvas)}>Boundary</button>
       <br />
       <br />
-      <form onSubmit={(e) => addImg(e, imgURL, canvas)}>
-        <div>
-          <input
-            type="text"
-            value={imgURL}
-            onChange={(e) => setImgURL(e.target.value)}
-          />
-          <button type="submit">Add Image</button>
-        </div>
-      </form>
-
       <br />
-      <br />
-      <canvas id="canvas" ref={canvasRef} />
       <video
         id="video"
         src="https://www.w3schools.com/html/mov_bbb.mp4"
         width="500"
         height="300"
         controls
+        ref={videoRef}
       />
+      <div
+        id="canvasWrapper"
+        style={{
+          position: "absolute",
+          width: 500,
+          height: 300,
+          left: 0,
+          top: 72,
+        }}
+      >
+        <canvas id="canvas" ref={canvasRef} width="500" height="300" />
+        <canvas
+          style={{ backgroundColor: "red" }}
+          id="canvas2"
+          ref={canvasRef2}
+          width="500"
+          height="300"
+        />
+      </div>
     </div>
   );
 };
